794d963f58ed9e4abfa487ca935320b0463d82e1
irietoshinari <60064033+irietoshinari@users.noreply.github.com>
2020-05-19 05:13:28 +0900

    userとstyleの実装

---
+++ app/controllers/styles_controller.rb
@@ -0,0 +1,44 @@
+class StylesController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+  def new
+    @style = Style.new
+  end
+  def create
+    @style = current_user.styles.build(style_params)
+    if @style.save
+      redirect_to style_path(@style), notice: "スタイルを投稿しました。"
+    else
+      render :new
+    end
+  end
+  def index
+    @styles = Style.all.order(id: "DESC")
+  end
+  def show
+    @style = Style.find(params[:id])
+  end
+  def edit
+    @style = Style.find(params[:id])
+    if @style.user != current_user
+        redirect_to styles_path, alert: "不正なアクセスです。"
+    end
+  end
+  def update
+    @style = Style.find(params[:id])
+    if @style.update(style_params)
+      redirect_to style_path(@style), notice: "スタイルを更新しました。"
+    else
+      render :edit
+    end
+  end
+  def destroy
+    style = Style.find(params[:id])
+    style.destroy
+    redirect_to user_path(style.user), notice: "スタイルを削除しました。"
+  end
+
+  private
+  def style_params
+    params.require(:style).permit(:title, :body, :image, :comment)
+  end
+end
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '0d1b224ee857c6fe35dcb2a354dfa463435cad72c26c644547c8733d44dde88221c6856fe7002fc2c850af8fd575cc0e0af64a1c883f006e3a3c6622b534f7ad'
